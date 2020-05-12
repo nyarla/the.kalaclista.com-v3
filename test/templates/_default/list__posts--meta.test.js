e4c459df13e52fa761d36ca00621eca3ce2472bc
nyarla <nyarla@kalaclista.com>
2020-05-12 09:53:00 +0900

    wip: wrote contents of head element on section home pages

--- test/templates/_default/list__posts--meta.test.js
+++ test/templates/_default/list__posts--meta.test.js
@@ -53,4 +53,41 @@ describe("posts/ -> _default/list.html # meta", () => {
       assert.equal($(test.selector).attr(test.attr), test.value);
     });
   });
+
+  it('script[type="application/json+ld"]', () => {
+    const $ = html("posts/index.html");
+
+    const jsonld = JSON.parse($('script[type="application/json+ld"]').html());
+    const data = jsonld[0];
+    const breadcrumb = jsonld[1];
+    const root = breadcrumb.itemListElement[0];
+    const branch = breadcrumb.itemListElement[1];
+
+    assert.equal(data["@context"], "https://schema.org");
+
+    assert.deepEqual(data.author, {
+      "@type": "Person",
+      name: "OKAMURA Naoki a.k.a nyarla",
+      email: "nyarla@kalaclista.com",
+    });
+
+    assert.equal(data["@id"], "https://the.kalaclista.com/posts/");
+    assert.equal(
+      data["headline"],
+      $('meta[property="og:title"]').attr("content")
+    );
+
+    assert.equal(breadcrumb["@context"], "https://schema.org");
+    assert.equal(breadcrumb["@type"], "BreadcrumbList");
+
+    assert.equal(root["@type"], "ListItem");
+    assert.equal(root.position, 1);
+    assert.equal(root.name, "カラクリスタ");
+    assert.equal(root.item, "https://the.kalaclista.com/");
+
+    assert.equal(branch["@type"], "ListItem");
+    assert.equal(branch.position, 2);
+    assert.equal(branch.name, data.headline);
+    assert.equal(branch.item, data["@id"]);
+  });
 });
